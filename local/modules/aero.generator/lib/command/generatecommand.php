fcbc169022c2fd2e1e0a6392d82d56c5c18caac7
maxim <maxim.loboda@gmail.com>
2017-08-26 01:44:28 +0300

    interface example

--- local/modules/aero.generator/lib/command/generatecommand.php
+++ local/modules/aero.generator/lib/command/generatecommand.php
@@ -5,6 +5,7 @@ namespace Aero\Generator\Command;
 use Notamedia\ConsoleJedi\Application\Command\BitrixCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 /**
  * Created by PhpStorm.
@@ -23,5 +24,31 @@ class GenerateCommand extends BitrixCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('<info>Yohoho!</info>');
+
+        // create a new progress bar (10 units)
+        $progress = new ProgressBar($output, 10);
+
+        // start and displays the progress bar
+        $progress->start();
+
+        $i = 0;
+        while ($i++ < 10) {
+
+            sleep(1);
+            // ... do some work
+
+            // advance the progress bar 1 unit
+            $progress->advance();
+
+            // you can also advance the progress bar by more than 1 unit
+            // $progress->advance(3);
+        }
+
+        // ensure that the progress bar is at 100%
+        $progress->finish();
+
+        echo PHP_EOL;
+
+        return 0;
     }
 }
